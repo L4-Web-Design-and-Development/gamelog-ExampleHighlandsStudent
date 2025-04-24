1beb2f1b3ae90a9f89c72518d4a2866f8a88685b
Ged Sargent <120935232+GedSargent@users.noreply.github.com>
2025-04-24 11:47:27 +0100

    create add game basic page with title

---
+++ app/routes/add-game.tsx
@@ -0,0 +1,9 @@
+export default function AddGame() {
+  return (
+    <div className="container mx-auto py-20">
+      <h1 className="font-bold text-9xl text-center">
+        Add <span className="text-orange-600">Game</span>
+      </h1>
+    </div>
+  );
+}
