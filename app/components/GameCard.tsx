c5bf797c06f32d43cbfa3b28ba725fede52658da
Ged Sargent <120935232+GedSargent@users.noreply.github.com>
2025-05-06 22:13:16 +0100

    style: wrap genre and release date in single div on GameCard

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -18,12 +18,14 @@ export default function GameCard({ title, releaseDate, genre }: GameCardProps) {
       <div className="flex justify-between">
         <div className="flex flex-col justify-between w-2/3">
           <h3 className="font-bold text-2xl text-slate-300">{title}</h3>
-          <p className="text-cyan-300 uppercase text-sm font-semibold">
-            {genre}
-          </p>
-          <p className="text-slate-200/60 text-sm font-semibold">
-            {formattedDate}
-          </p>
+          <div>
+            <p className="text-cyan-300 uppercase text-sm font-semibold mb-1">
+              {genre}
+            </p>
+            <p className="text-slate-200/60 text-sm font-semibold">
+              {formattedDate}
+            </p>
+          </div>
         </div>
         <div className="flex flex-col gap-7 w-1/3">
           <button className="border-2 border-cyan-300 text-cyan-300 p-2 rounded-md">
