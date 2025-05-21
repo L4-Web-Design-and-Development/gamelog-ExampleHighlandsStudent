4f57a93a265e5921cf8f3a05995c80bc96f78f5a
Ged Sargent <120935232+GedSargent@users.noreply.github.com>
2025-05-21 18:39:49 +0100

    feat: add link to 'edit-game' page from GameCard image

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Form } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 
 interface GameCardProps {
   id: string;
@@ -18,13 +18,13 @@ export default function GameCard({
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="relative h-72 overflow-hidden">
+      <Link to={`/edit-game/${id}`} className="relative h-72 overflow-hidden">
         <img
           src={imageUrl}
           alt={`${title} cover`}
           className="absolute inset-0 w-full h-full object-cover rounded-xl"
         />
-      </div>
+      </Link>
       <div className="flex justify-between">
         <div className="flex flex-col justify-between w-2/3 pr-4">
           <h3 className="font-bold text-2xl text-slate-300">{title}</h3>
